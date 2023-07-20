22f4ddf1f62def766d9c94042368f64c85234405
z4l1nux <124527204+z4l1nux@users.noreply.github.com>
2023-07-20 16:10:21 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+port = 3000
+
+app.get('/', function (req, res) {
+    res.send('<h1>Bem-vindas(os)! Oracle Cloud Infrastructure DevOps Version 1!!! DevOps Experience & DevOps Heroes!!! </h1>')
+})
+
+app.listen(port, function() {
+    console.log('OKE - Oracle Cloud app listening on port ' + port);
+})                   
